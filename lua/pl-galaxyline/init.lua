3610abc67815ef9807029dbe77f032fbfbfa5fc1
thuando <thuan14121999@gmail.com>
2021-07-17 22:17:56 +0700

    set up the galaxyline - statusline

--- lua/pl-galaxyline/init.lua
+++ lua/pl-galaxyline/init.lua
@@ -1,336 +1,239 @@
--- if not package.loaded['galaxyline'] then
---   return
--- end
+local vim = vim
+local gl = require('galaxyline')
+local utils = require('utils')
 
-local gl = require "galaxyline"
--- get my theme in galaxyline repo
--- local colors = require('galaxyline.theme').default
+local gls = gl.section
+gl.short_line_list = { 'defx', 'packager', 'vista' }
+
+-- Colors
 local colors = {
-  bg = "#fff",
-  yellow = "#DCDCAA",
-  dark_yellow = "#D7BA7D",
-  cyan = "#4EC9B0",
-  green = "#608B4E",
-  light_green = "#B5CEA8",
-  string_orange = "#CE9178",
-  orange = "#FF8800",
-  purple = "#C586C0",
-  magenta = "#D16D9E",
-  grey = "#858585",
-  blue = "#569CD6",
-  vivid_blue = "#4FC1FF",
-  light_blue = "#9CDCFE",
-  red = "#D16969",
-  error_red = "#F44747",
-  info_yellow = "#FFCC66",
+  bg = '#282a36',
+  fg = '#f8f8f2',
+  section_bg = '#38393f',
+  yellow = '#f1fa8c',
+  cyan = '#8be9fd',
+  green = '#50fa7b',
+  orange = '#ffb86c',
+  magenta = '#ff79c6',
+  blue = '#8be9fd',
+  red = '#ff5555'
 }
 
--- galaxyline themes for Gruvbox and NVCode.
--- Uncomment and change 'colors_colorschemeName'
--- to 'colors' to enable.
-
--- Colors for Gruvbox
--- local colors_gruvbox = {
---     bg = '#32302F',
---     yellow = '#FABD2F',
---     dark_yellow = '#D79921',
---     cyan = '#689D6A',
---     green = '#608B4E',
---     light_green = '#B8BB26',
---     string_orange = '#D65D0E',
---     orange = '#FE8019',
---     purple = '#B16286',
---     magenta = '#D3869B',
---     grey = '#A89984',
---     blue = '#458588',
---     -- vivid_blue = '#4FC1FF',
---     light_blue = '#83A598',
---     red = '#FB4834',
---     error_red = '#CC241D',
---     info_yellow = '#D79921'
--- }
--- colors for NVCode theme (very minimal changes)
--- local colors_nvcode = {
---     bg = '#2E2E2E',
---     yellow = '#DCDCAA',
---     dark_yellow = '#D7BA7D',
---     cyan = '#4EC9B0',
---     green = '#608B4E',
---     light_green = '#B5CEA8',
---     string_orange = '#CE9178',
---     orange = '#FF8800',
---     purple = '#C586C0',
---     magenta = '#D16D9E',
---     grey = '#729CB3',
---     blue = '#569CD6',
---     vivid_blue = '#4FC1FF',
---     light_blue = '#9CDCFE',
---     red = '#D16969',
---     error_red = '#F44747',
---     info_yellow = '#FFCC66'
--- }
-
-local condition = require "galaxyline.condition"
-local gls = gl.section
-gl.short_line_list = { "NvimTree", "vista", "dbui", "packer" }
-
-table.insert(gls.left, {
-  ViMode = {
-    provider = function()
-      -- auto change color according the vim mode
-      local mode_color = {
-        n = colors.blue,
-        i = colors.green,
-        v = colors.purple,
-        [""] = colors.purple,
-        V = colors.purple,
-        c = colors.magenta,
-        no = colors.blue,
-        s = colors.orange,
-        S = colors.orange,
-        [""] = colors.orange,
-        ic = colors.yellow,
-        R = colors.red,
-        Rv = colors.red,
-        cv = colors.blue,
-        ce = colors.blue,
-        r = colors.cyan,
-        rm = colors.cyan,
-        ["r?"] = colors.cyan,
-        ["!"] = colors.blue,
-        t = colors.blue,
-      }
-      vim.api.nvim_command("hi GalaxyViMode guifg=" .. mode_color[vim.fn.mode()])
-      return "▊"
-    end,
-    -- highlight = 'TabLineSel'
-    -- highlight = {colors.red, colors.bg}
-  },
-})
--- print(vim.fn.getbufvar(0, 'ts'))
-vim.fn.getbufvar(0, "ts")
-
-table.insert(gls.left, {
-  GitIcon = {
-    provider = function()
-      return "  "
-    end,
-    condition = condition.check_git_workspace,
-    separator = " ",
-    separator_highlight = "StatusLineSeparator",
-    highlight = "StatusLineGit",
-  },
-})
-
-table.insert(gls.left, {
-  GitBranch = {
-    provider = "GitBranch",
-    condition = condition.check_git_workspace,
-    separator = " ",
-    separator_highlight = "StatusLineSeparator",
-    highlight = "StatusLineNC",
-  },
-})
-
-table.insert(gls.left, {
-  DiffAdd = {
-    provider = "DiffAdd",
-    condition = condition.hide_in_width,
-    icon = "  ",
-    -- highlight = "StatusLineGitAdd",
-    highlight = {colors.green, colors.bg},
-  },
-})
-
-table.insert(gls.left, {
-  DiffModified = {
-    provider = "DiffModified",
-    condition = condition.hide_in_width,
-    icon = " 柳",
-    -- highlight = "StatusLineGitChange",
-	highlight = {colors.blue, colors.bg}
-  },
-})
-
-table.insert(gls.left, {
-  DiffRemove = {
-    provider = "DiffRemove",
-    condition = condition.hide_in_width,
-    icon = "  ",
-    -- highlight = "StatusLineGitDelete",
-    highlight = {colors.red, colors.bg},
-  },
-})
-
-table.insert(gls.right, {
-  DiagnosticError = {
-    provider = "DiagnosticError",
-    icon = "  ",
-    highlight = "StatusLineLspDiagnosticsError",
-  },
-})
-table.insert(gls.right, {
-  DiagnosticWarn = {
-    provider = "DiagnosticWarn",
-    icon = "  ",
-
-    highlight = "StatusLineLspDiagnosticsWarning",
-  },
-})
-
-table.insert(gls.right, {
-  DiagnosticInfo = {
-    provider = "DiagnosticInfo",
-    icon = "  ",
-
-    highlight = "StatusLineLspDiagnosticsInformation",
-  },
-})
-
-table.insert(gls.right, {
-  DiagnosticHint = {
-    provider = "DiagnosticHint",
-    icon = "  ",
-
-    highlight = "StatusLineLspDiagnosticsHint",
-  },
-})
-
-table.insert(gls.right, {
-  TreesitterIcon = {
-    provider = function()
-      if next(vim.treesitter.highlighter.active) ~= nil then
-        return "  "
-      end
-      return ""
-    end,
-    separator = " ",
-    separator_highlight = "StatusLineSeparator",
-    highlight = "StatusLineTreeSitter",
-  },
-})
-
-local get_lsp_client = function(msg)
-  msg = msg or "No Active LSP Client"
-  local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-  local clients = vim.lsp.get_active_clients()
-  if next(clients) == nil then
-    return msg
-  end
-  local lsps = ""
-  for _, client in ipairs(clients) do
-    local filetypes = client.config.filetypes
-    if filetypes and vim.fn.index(filetypes, buf_ft) ~= 1 then
-      -- print(client.name)
-      if lsps == "" then
-        -- print("first", lsps)
-        lsps = client.name
-      else
-        lsps = lsps .. ", " .. client.name
-        -- print("more", lsps)
-      end
-    end
-  end
-  if lsps == "" then
-    return msg
-  else
-    return lsps
-  end
+-- Local helper functions
+local buffer_not_empty = function()
+  return not utils.is_buffer_empty()
 end
 
-table.insert(gls.right, {
-  ShowLspClient = {
-    provider = get_lsp_client,
-    condition = function()
-      local tbl = { ["dashboard"] = true, [" "] = true }
-      if tbl[vim.bo.filetype] then
-        return false
+local in_git_repo = function ()
+  local vcs = require('galaxyline.provider_vcs')
+  local branch_name = vcs.get_git_branch()
+
+  return branch_name ~= nil
+end
+
+local checkwidth = function()
+  return utils.has_width_gt(40) and in_git_repo()
+end
+
+local mode_color = function()
+  local mode_colors = {
+    n = colors.cyan,
+    i = colors.green,
+    c = colors.orange,
+    V = colors.magenta,
+    [''] = colors.magenta,
+    v = colors.magenta,
+    R = colors.red,
+  }
+
+  local color = mode_colors[vim.fn.mode()]
+
+  if color == nil then
+    color = colors.red
+  end
+
+  return color
+end
+
+-- Left side
+gls.left[1] = {
+  FirstElement = {
+    provider = function() return '▋' end,
+    highlight = { colors.cyan, colors.section_bg }
+  },
+}
+gls.left[2] = {
+  ViMode = {
+    provider = function()
+      local alias = {
+        n = 'NORMAL',
+        i = 'INSERT',
+        c = 'COMMAND',
+        V = 'VISUAL',
+        [''] = 'VISUAL',
+        v = 'VISUAL',
+        R = 'REPLACE',
+      }
+      vim.api.nvim_command('hi GalaxyViMode guifg='..mode_color())
+      local alias_mode = alias[vim.fn.mode()]
+      if alias_mode == nil then
+        alias_mode = vim.fn.mode()
       end
-      return true
+      return alias_mode..' '
     end,
-    icon = "  ",
-    highlight = "StatusLineNC",
+    highlight = { colors.bg, colors.bg },
+    separator = "  ",
+    separator_highlight = {colors.bg, colors.section_bg},
   },
-})
-
-table.insert(gls.right, {
-  LineInfo = {
-    provider = "LineColumn",
-    separator = "  ",
-    separator_highlight = "StatusLineSeparator",
-    highlight = "StatusLineNC",
+}
+gls.left[3] ={
+  FileIcon = {
+    provider = 'FileIcon',
+    condition = buffer_not_empty,
+    highlight = { require('galaxyline.provider_fileinfo').get_file_icon_color, colors.section_bg },
   },
-})
-
-table.insert(gls.right, {
-  PerCent = {
-    provider = "LinePercent",
-    separator = " ",
-    separator_highlight = "StatusLineSeparator",
-    highlight = "StatusLineNC",
-  },
-})
-
-table.insert(gls.right, {
-  Tabstop = {
+}
+gls.left[4] = {
+  FileName = {
+    provider = 'FileName',
+    condition = buffer_not_empty,
+    highlight = { colors.fg, colors.section_bg },
+    separator = " ",
+    separator_highlight = {colors.section_bg, colors.bg},
+  }
+}
+gls.left[5] = {
+  GitIcon = {
+    provider = function() return '  ' end,
+    condition = in_git_repo,
+    highlight = {colors.red,colors.bg},
+  }
+}
+gls.left[6] = {
+  GitBranch = {
     provider = function()
-      return "Spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth") .. " "
+      local vcs = require('galaxyline.provider_vcs')
+      local branch_name = vcs.get_git_branch()
+      if (string.len(branch_name) > 28) then
+        return string.sub(branch_name, 1, 25).."..."
+      end
+      return branch_name .. " "
     end,
-    condition = condition.hide_in_width,
-    separator = " ",
-    separator_highlight = "StatusLineSeparator",
-    highlight = "StatusLineNC",
-  },
-})
-
-table.insert(gls.right, {
-  BufferType = {
-    provider = "FileTypeName",
-    condition = condition.hide_in_width,
-    separator = " ",
-    separator_highlight = "StatusLineSeparator",
-    highlight = "StatusLineNC",
-  },
-})
-
-table.insert(gls.right, {
-  FileEncode = {
-    provider = "FileEncode",
-    condition = condition.hide_in_width,
-    separator = " ",
-    separator_highlight = "StatusLineSeparator",
-    highlight = "StatusLineNC",
-  },
-})
-
-table.insert(gls.right, {
+    condition = in_git_repo,
+    highlight = {colors.fg,colors.bg},
+  }
+}
+gls.left[7] = {
+  DiffAdd = {
+    provider = 'DiffAdd',
+    condition = checkwidth,
+    icon = ' ',
+    highlight = { colors.green, colors.bg },
+  }
+}
+gls.left[8] = {
+  DiffModified = {
+    provider = 'DiffModified',
+    condition = checkwidth,
+    icon = ' ',
+    highlight = { colors.orange, colors.bg },
+  }
+}
+gls.left[9] = {
+  DiffRemove = {
+    provider = 'DiffRemove',
+    condition = checkwidth,
+    icon = ' ',
+    highlight = { colors.red,colors.bg },
+  }
+}
+gls.left[10] = {
+  LeftEnd = {
+    provider = function() return ' ' end,
+    condition = buffer_not_empty,
+    highlight = {colors.section_bg,colors.bg}
+  }
+}
+gls.left[11] = {
+  DiagnosticError = {
+    provider = 'DiagnosticError',
+    icon = '  ',
+    highlight = {colors.red,colors.section_bg}
+  }
+}
+gls.left[12] = {
   Space = {
-    provider = function()
-      return " "
-    end,
-    separator = " ",
-    separator_highlight = "StatusLineSeparator",
-    highlight = "StatusLineNC",
-  },
-})
+    provider = function () return ' ' end,
+    highlight = {colors.section_bg,colors.section_bg},
+  }
+}
+gls.left[13] = {
+  DiagnosticWarn = {
+    provider = 'DiagnosticWarn',
+    icon = '  ',
+    highlight = {colors.orange,colors.section_bg},
+  }
+}
+gls.left[14] = {
+  Space = {
+    provider = function () return ' ' end,
+    highlight = {colors.section_bg,colors.section_bg},
+  }
+}
+gls.left[15] = {
+  DiagnosticInfo = {
+    provider = 'DiagnosticInfo',
+    icon = '  ',
+    highlight = {colors.blue,colors.section_bg},
+    separator = ' ',
+    separator_highlight = { colors.section_bg, colors.bg },
+  }
+}
 
-table.insert(gls.short_line_left, {
+-- Right side
+gls.right[1]= {
+  FileFormat = {
+    provider = function() return vim.bo.filetype end,
+    highlight = { colors.fg,colors.section_bg },
+    separator = ' ',
+    separator_highlight = { colors.section_bg,colors.bg },
+  }
+}
+gls.right[2] = {
+  LineInfo = {
+    provider = 'LineColumn',
+    highlight = { colors.fg, colors.section_bg },
+    separator = ' | ',
+    separator_highlight = { colors.bg, colors.section_bg },
+  },
+}
+-- gls.right[3] = {
+--   Heart = {
+--     provider = function() return ' ' end,
+--     highlight = { colors.red, colors.section_bg },
+--     separator = ' | ',
+--     separator_highlight = { colors.bg, colors.section_bg },
+--   }
+-- }
+
+-- Short status line
+gls.short_line_left[1] = {
   BufferType = {
-    provider = "FileTypeName",
-    separator = " ",
-    separator_highlight = "StatusLineSeparator",
-    highlight = "StatusLineNC",
-  },
-})
+    provider = 'FileTypeName',
+    highlight = { colors.fg, colors.section_bg },
+    separator = ' ',
+    separator_highlight = { colors.section_bg, colors.bg },
+  }
+}
 
-table.insert(gls.short_line_left, {
-  SFileName = {
-    provider = "SFileName",
-    condition = condition.buffer_not_empty,
+gls.short_line_right[1] = {
+  BufferIcon = {
+    provider= 'BufferIcon',
+    highlight = { colors.yellow, colors.section_bg },
+    separator = ' ',
+    separator_highlight = { colors.section_bg, colors.bg },
+  }
+}
 
-    highlight = "StatusLineNC",
-  },
-})
-
---table.insert(gls.short_line_right[1] = {BufferIcon = {provider = 'BufferIcon', highlight = {colors.grey, colors.bg}}})
-
+-- Force manual load so that nvim boots with a status line
+gl.load_galaxyline()
